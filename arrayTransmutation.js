a33464b25c138ae4f510a326db982de6b06964af
Thomassaiag <thomas.saiag@gmail.com>
2025-04-01 15:59:38 +0200

    added comments

--- arrayTransmutation.js
+++ arrayTransmutation.js
@@ -2,18 +2,19 @@ const transmuteArray = (grid, rules) => {
 	const gridWidth = grid[0].length;
 	const gridHeight = grid.length;
 	let finalTable = [];
+    // we iterate over the grid Height
 	for (let y = 0; y < gridHeight - 1; y++) {
 		let newLine = [];
 		for (let x = 0; x < gridWidth - 1; x++) {
 			let tableToTransmute = [];
-
+            //we push the 4 value of the current table
 			tableToTransmute.push(
 				grid[y][x],
 				grid[y][x + 1],
 				grid[y + 1][x],
 				grid[y + 1][x + 1]
 			);
-
+            //we check that this table exists into rule
 			let ruleToApply = rules.find((rule) => {
 				return (
 					JSON.stringify(rule["pattern"]) ===
@@ -22,9 +23,10 @@ const transmuteArray = (grid, rules) => {
 			});
 
 			let resultToAdd = ruleToApply ? parseInt(ruleToApply.rule) : 0;
-
+            //we push the new values into a line table
 			newLine.push(resultToAdd);
 		}
+        //we push the linetable into the final one
 		finalTable.push(newLine);
 	}
 	return finalTable;
