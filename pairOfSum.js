c3dd12a00bb3873d61731ea4d848c1d8dcfe060e
Thomassaiag <thomas.saiag@gmail.com>
2025-04-01 16:01:45 +0200

    added comments

--- pairOfSum.js
+++ pairOfSum.js
@@ -3,14 +3,17 @@ const findPairIndex = (array, target) => {
 	const arrayLength = array.length;
 	const finalList = [];
 	for (let i = 0; i < arrayLength; i++) {
+        //we check the difference for each table value
 		let difference = target - array[i];
-
+        // if the difference is part of the seen values, we have a matching sum and can return value indexes into an array
 		if (seen.has(difference)) {
 			finalList.push(array.indexOf(difference), array.indexOf(array[i]));
 			return finalList;
 		}
+        // if not, we add the current value to the set. We'll check if this value is a difference on the next iteration
 		seen.add(array[i]);
 	}
+    // if there's no matching pair, we return null
 	return null;
 };
 
